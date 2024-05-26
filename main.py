7a2c8faa901f7a884d921de14d3fe5b689767e72
Ollie Lloyd <oliver.lloyd@bristol.ac.uk>
2024-05-26 14:12:35 +0100

    Temp solution for 3d viz

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from simple_biosnap import Embeds
 import streamlit as st
 import altair as alt
-
+import matplotlib.pyplot as plt
 
 # Header
 st.title('SimplE embeddings of BioSNAP drugs')
 st.link_button(
-    'Raw/full data avilable here',
+    'Full 256-dimension data avilable here',
     url='https://github.com/oliver-lloyd/SimplE_BioSNAP/blob/master/data/raw_embed_df.csv'
 )
 
@@ -44,7 +44,20 @@ if n <= 2:
         use_container_width=True
     )
 elif n == 3:
-    st.write(f'Sorry, 3d visualisation is a work in progress.')
+    st.write(f'3D visualisation is a work in progress. For the time being, enjoy this non-interactable pyplot figure:')
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(projection='3d')
+    ax.scatter(
+        xs='Component 1',
+        ys='Component 2',
+        zs='Component 3',
+        data=emb.pca_df, 
+        marker='x'
+    )
+    ax.set_xlabel('Component 1')
+    ax.set_ylabel('Component 2')
+    ax.set_zlabel('Component 3')
+    st.pyplot(fig, use_container_width=True)
 else:
     st.write(f'Cannot visualise in {n} dimensions.')
 
@@ -57,4 +70,5 @@ st.write('- BioSNAP dataset: http://snap.stanford.edu/decagon')
 st.write('- Decagon paper: https://doi.org/10.1093/bioinformatics/bty294')
 st.write('- LibKGE: https://github.com/uma-pi1/kge')
 st.write('- SimplE paper: https://doi.org/10.48550/arXiv.1802.04868')
+st.write('')
 st.write('Contact: oliver.lloyd@bristol.ac.uk, https://github.com/oliver-lloyd')
