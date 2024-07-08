f76e1d6bd8ad35c22f9f04caaf6ddd4829568953
Ollie Lloyd <oliver.lloyd@bristol.ac.uk>
2024-07-08 10:24:25 +0100

    Real scores with relation vectors

--- pages/1_Edge_Scores.py
+++ pages/1_Edge_Scores.py
@@ -1,43 +1,42 @@
 import streamlit as st
+
+
 # Initialize
 emb = st.session_state.emb
 
 # Header
-st.title('[Work in progress] SimplE polypharmacy side effect scores')
+st.title('SimplE polypharmacy side effect scores')
 
 
 # Body
 st.write('\
-    This page will allow users to calculate edge scores, \
+    This page allows users to calculate edge scores, \
     i.e. model confidence scores of polyparmacy side \
     effects, between any pair of drugs in the dataset. \
 ')
 st.write('\
     Please note that this tool is intended for research \
     purposes only. Consult with a pharmacist if you have \
-    any real queries about side effects. \
+    any real queries about drug side effects. \
 ')
 
 gap = 2
 for _ in range(gap):
     st.write('')
 st.write('Choose two drugs:')
-drug1 = st.selectbox('Drug 1', emb.index['Drug'])
-drug2 = st.selectbox('Drug 2', emb.index['Drug'])
+drug1 = st.selectbox('Drug 1', emb.ent_index['Drug'])
+drug2 = st.selectbox('Drug 2', emb.ent_index['Drug'])
 
 for _ in range(gap):
     st.write('')
 st.write('Choose a side effect')
-se_options = ['All']
-se = st.selectbox('Side effect', se_options + ['placeholder1', 'placeholder2', 'placeholder3'])
+se = st.selectbox('Side effect', emb.rel_index['relation'])
 
 for _ in range(gap):
     st.write('')
 if drug1 != drug2:
-    if se != 'All':
-        score = emb.SimplE_scorer(drug1, se, drug2)
-        st.write(f'Score of triple ({drug1}, {se}, {drug2}):')
-        st.write(f'{score:.3f}')
-        st.write('Please note that this is just a working example (not a real score) until I can add the side effect embeddings to the project.')
+    score = emb.SimplE_scorer(drug1, se, drug2)
+    st.write(f'Score of triple ({drug1}, {se}, {drug2}):')
+    st.write(f'{score:.3f}')
 else:
     st.write('Choose two different drugs to get a score.')
--- simple_biosnap.py
+++ simple_biosnap.py
@@ -2,56 +2,64 @@ import pandas as pd
 
 class Embeds:
 
-    def __init__(self, embeds_path='data/raw_embed_df.csv', drug_info_path='data/pubchem_info.csv'):
+    def __init__(self, embeds_path='data/raw_embed_df.csv', drug_info_path='data/pubchem_info.csv', rel_embeds_path='data/rel_embeds.csv'):
         
-        self.raw_path = embeds_path
+        self.ent_embed_path = embeds_path
         self.drug_info = pd.read_csv(drug_info_path)
+        self.rel_embed_path = rel_embeds_path
         self.load_raw()
 
 
     def load_raw(self):
-        self.raw_df = pd.read_csv(self.raw_path)
-        self.index = self.raw_df[['selfloops_index','Drug']]
+        ent_raw_df = pd.read_csv(self.ent_embed_path)
+        self.ent_index = ent_raw_df[['selfloops_index','Drug']]
         self._format_drugnames()
 
-        self.full_dim = int(self.raw_df.columns[-1])
+        rel_raw_df = pd.read_csv(self.rel_embed_path)
+        filter_rels = ['DrugTarget', 'ProteinProteinInteraction']
+        rel_raw_df.query('relation not in @filter_rels', inplace=True)
+        rel_raw_df.reset_index(inplace=True, drop=True)
+        self.rel_index = rel_raw_df[['relation']]
+
+        self.full_dim = int(ent_raw_df.columns[-1])
         self.emb_colnames = [str(i) for i in range(self.full_dim)]
-        self.emb_ = self.raw_df[self.emb_colnames].to_numpy()
+        self.ent_emb_ = ent_raw_df[self.emb_colnames].to_numpy()
+        self.rel_emb_ = rel_raw_df[self.emb_colnames].to_numpy()
         
 
     def pca(self, n_dim=3):
         from sklearn.decomposition import PCA
 
         self.pca_dim = n_dim
-        self.pca_obj = PCA(n_dim).fit(self.emb_.T)
+        self.pca_obj = PCA(n_dim).fit(self.ent_emb_.T)
         self.pca_emb_ = self.pca_obj.components_.T
         cols = [f'Component {n+1}' for n in range(n_dim)]
         pca_df = pd.DataFrame(self.pca_emb_, columns=cols)
         pca_df = pd.concat(
-            [self.index['Drug'], pca_df], 
+            [self.ent_index['Drug'], pca_df], 
             axis=1
         )
         self.pca_df = pca_df.merge(self.drug_info, on='Drug')
 
 
     def _format_drugnames(self):
-        raw_strs = [d.split('CID')[1] for d in self.index['Drug'].values]
+        raw_strs = [d.split('CID')[1] for d in self.ent_index['Drug'].values]
         IDs = [int(st) for st in raw_strs]
-        formatted = [f'CID{id}' for id in IDs]
-        self.index['Drug'] = formatted
-        self.drug_info['Drug'] = formatted
+        formatted_drugs = [f'CID{id}' for id in IDs]
+        self.ent_index['Drug'] = formatted_drugs
+        self.drug_info['Drug'] = formatted_drugs
 
 
     def SimplE_scorer(self, ent1, rel, ent2):
         from numpy import dot, multiply
-        id1 = self.index.query('Drug == @ent1').index[0]
-        id2 = self.index.query('Drug == @ent2').index[0]
-        #rel_id = self.rel_emb_.query('SE == @rel').index[0]
 
-        vec1 = self.emb_[id1]
-        vec2 = self.emb_[id2]
-        #rel_vec = self.rel_emb_[rel_id]
+        id1 = self.ent_index.query('Drug == @ent1').index[0]
+        id2 = self.ent_index.query('Drug == @ent2').index[0]
+        rel_id = self.rel_index.query('relation == @rel').index[0]
 
-        #return np.dot(np.multiply(vec1, rel_vec), vec2)
-        return dot(multiply(vec1, vec2), vec2)  # Delete line when have proper rel embeds
+        vec1 = self.ent_emb_[id1]
+        vec2 = self.ent_emb_[id2]
+        rel_vec = self.rel_emb_[rel_id]
+
+        return dot(multiply(vec1, rel_vec), vec2)
     
