ffd3fa8a2afc6624cc82b7916ee24420c8edf1c3
Ollie Lloyd <oliver.lloyd@bristol.ac.uk>
2024-07-09 12:23:43 +0100

    minor fix

--- pages/2_Other_Models.py
+++ pages/2_Other_Models.py
@@ -4,7 +4,8 @@ import pandas as pd
 
 st.title('Other models and their performance on this dataset')
 st.write('')
-st.write('Please get in touch to report any inaccuracies on this page, or suggest papers I\'ve missed.)
+st.write('Please get in touch to report any inaccuracies on this page, or suggest papers I\'ve missed.')
+
 df = pd.read_excel('data/lit/biosnap_papers.xlsx')
 st.dataframe(
     df.drop(columns=['Paper']), 
