7c65d7da0d6c375a06c5b76b9d359218bd157741
Ollie Lloyd <oliver.lloyd@bristol.ac.uk>
2024-07-09 12:20:48 +0100

    Literature page

---
+++ pages/2_Other_Models.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+
+
+st.title('Other models and their performance on this dataset')
+st.write('')
+st.write('Please get in touch to report any inaccuracies on this page, or suggest papers I\'ve missed.)
+df = pd.read_excel('data/lit/biosnap_papers.xlsx')
+st.dataframe(
+    df.drop(columns=['Paper']), 
+    hide_index=True,
+    use_container_width=True
+)
